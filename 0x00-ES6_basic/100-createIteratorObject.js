ff35da43af62b516957dad79dee19ad52009d7c4
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-01 01:03:27 +0300

    Iterator Object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+  const { allEmployees } = report;
+  let employeesList = [];
+
+  for (const department in allEmployees) {
+    if (Object.hasOwnProperty.call(allEmployees, department)) {
+      employeesList = employeesList.concat(allEmployees[department]);
+    }
+  }
+  return employeesList[Symbol.iterator]();
+}
