776644c228b07db31b80ca4b19a679c7792f55b3
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-03 01:04:47 +0300

    catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-    promise.then(() => {
-        console.log("Got a response from the API");
-        return { status: 200, body: 'success' };
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
     })
-        .catch(() => {
-            console.log("Got a response from the API")
-            return {};
-        });
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
