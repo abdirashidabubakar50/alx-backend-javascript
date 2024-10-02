85a9823d921a34f3c8ae3750b93abee135d824e0
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-03 02:20:11 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, name]) => {
       console.log(`${photo.body} ${name.firstName} ${name.lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
