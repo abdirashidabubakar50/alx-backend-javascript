35efbcf064f431d31630c1e477511764be8a07e7
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-03 04:05:53 +0300

    handle multiple promises...Completed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((results) => {
-      results.forEach((result) => ({
-        status: result.status,
-        value: result.value,
-      }));
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          return {
+            status: result.status,
+            value: result.value,
+          };
+        }
+        return result.reason;
+      });
     });
 }
