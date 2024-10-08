479183724cec08b1202011eae0da09e58c9f9669
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-08 12:05:13 +0300

    Basic list of objects...Completed

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,13 +2,13 @@ export default function getListStudents() {
   const students = [];
   students.push(
     {
-      id: 1, firstname: 'Guillaume', location: 'San Fransisco',
+      id: 1, firstName: 'Guillaume', location: 'San Fransisco',
     },
     {
-      id: 2, firstname: 'James', location: 'Columbia',
+      id: 2, firstName: 'James', location: 'Columbia',
     },
     {
-      id: 5, firstname: 'Serena', location: 'San Francisco',
+      id: 5, firstName: 'Serena', location: 'San Francisco',
     },
   );
   return students;
