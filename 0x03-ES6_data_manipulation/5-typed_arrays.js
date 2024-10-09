2ff1ff1e8f7523dc3d37431a19710e089e91c86c
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-10 00:34:34 +0300

    Typed Arrays...Completed

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,17 @@
+export default function createInt8TypedArray(length, position, value) {
+  /* *returns a new ArrayBUffer with Int8 value at a specific location */
+  const buffer = new ArrayBuffer(length);
+
+  // create a Dataview for the ArrayBuffer
+  const view = new DataView(buffer);
+  // check if position is vaild otherwise throw an error
+  if (position < 0 || position >= length) {
+    throw new Error('Postition outside range');
+  }
+
+  // set  the int8 value ar the specified position
+  view.setInt16(position, value);
+
+  // Return the DataView for the  ArrayBuffer
+  return view;
+}
