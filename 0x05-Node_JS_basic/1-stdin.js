caf3cb15fdb106d3fe9dc91bea28e224ae5b25ad
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-02 11:16:59 +0300

    using process.stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf-8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+// Listen for user input
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`your name is: ${name}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
