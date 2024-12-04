dd9272cf7e5a456955a1eab9d017c0fd4cf0cb27
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-04 19:35:12 +0300

    using  process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,9 +4,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
+  process.stdin.end();
 });
 
-process.on('exit', () => {
+process.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
