3fcb9de410a4ce4df0af5032ba09c36982faf7a7
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-04 19:56:51 +0300

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for user input
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}\n`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
