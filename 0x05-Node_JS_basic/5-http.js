a7585d4b4c968473f9d2bb00ef633158c605bd55
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-06 17:06:48 +0300

    creating complex Http server using node js http module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,36 +1,44 @@
 const http = require('http');
-// const url = require('url');
 const path = require('path');
 const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello Holberton School!');
-        } else if (req.url === '/students') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('This is the list of our students:\n');
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      const responseMessage = ['This is the list of our students'];
 
-        const databasePath = path.resolve(process.argv[2]);
+      const databasePath = path.resolve(process.argv[2]);
 
-        countStudents(databasePath)
-            .then((output) => {
-                res.end(output);
-            })
-            .catch((error) => {
-                res.end(error.message)
-            });
-        } else {
-            res.writeHead(404, 'Content-Type', 'text/plain');
-            res.end('Route not found');
-        }
+      const captureOutput = [];
+      const originalLog = console.log;
+      console.log = (message) => captureOutput.push(message);
+
+      countStudents(databasePath)
+        .then(() => {
+          responseMessage.push(...captureOutput);
+          res.end(responseMessage.join('\n'));
+        })
+        .catch((error) => {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(error.message);
+        })
+        .finally(() => {
+          console.log = originalLog; // Restore the original console.log
+        });
     } else {
-        res.writeHead(404, 'Content-type', 'text/plain')
-        res.end('Method not allowed')
+      res.writeHead(404, 'Content-Type', 'text/plain');
+      res.end('Route not found');
     }
+  } else {
+    res.writeHead(405, 'Content-type', 'text/plain');
+    res.end('Method not allowed');
+  }
 });
 
 app.listen(port, () => {
