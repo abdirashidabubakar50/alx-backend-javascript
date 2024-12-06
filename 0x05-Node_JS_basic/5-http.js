73d44f2db2a7b6c96a8c2f23702b70aedca0fbff
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-06 16:20:57 +0300

    small server using http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,39 +6,31 @@ const countStudents = require('./3-read_file_async');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  try {
     if (req.method === 'GET') {
-      if (req.url === '/') {
+        if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello Holberton School!');
-      } else if (req.url === '/students') {
+        } else if (req.url === '/students') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('This is the list of our students:\n');
 
         const databasePath = path.resolve(process.argv[2]);
 
-        // Redirect console logs to a custom stream
-        const originalLog = console.log;
-        const logs = [];
-        console.log = (message) => logs.push(message);
-
         countStudents(databasePath)
-          .then(() => {
-            console.log = originalLog;
-            res.end(logs.join('\n'));
-          })
-          .catch((error) => {
-            console.log = originalLog;
-            res.end(error.message);
-          });
-      } else {
-        res.writeHead(404, 'Content-Type', 'text/plain');
-        res.end('Route not found');
-      }
+            .then((output) => {
+                res.end(output);
+            })
+            .catch((error) => {
+                res.end(error.message)
+            });
+        } else {
+            res.writeHead(404, 'Content-Type', 'text/plain');
+            res.end('Route not found');
+        }
+    } else {
+        res.writeHead(404, 'Content-type', 'text/plain')
+        res.end('Method not allowed')
     }
-  } catch (error) {
-    throw new Error('Route not found');
-  }
 });
 
 app.listen(port, () => {
