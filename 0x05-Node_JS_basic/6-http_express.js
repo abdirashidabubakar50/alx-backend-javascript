aa8a42a0393830944693f25a8d006ca1c4323b02
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-06 17:45:09 +0300

    creating a small http server using express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
-const port = 1245
+const port = 1245;
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
 
 module.exports = app;
