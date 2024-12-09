c271b5966941cb6a8d97d2a29086e38e917012bb
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-09 12:38:58 +0300

    spies using sinon.spy

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,25 +5,13 @@ const sendPaymentRequestToApi = require('./3-payments')
 describe('sendPaymentRequestToApi', () => {
 
     it('should validate usage of Utils.calculateNumber', () => {
-        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-        const consoleLogSpy = sinon.spy(console, "log");
+        const calculateNumberSpy = sinon.spy(Utils, "calculateNumber");
 
-        // call the function to test
         sendPaymentRequestToApi(100, 20);
 
-        // Validate the calculateNumber was called once
-        // sinon.assert.calledOnceWithExactly(calculateNumberSpy);
-        sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-
-        const expectedResult = Utils.calculateNumber("SUM", 100, 20);
-
-        sinon.assert.calledWithExactly(
-          consoleLogSpy,
-          `The total is: ${expectedResult}`
-        );
+        sinon.assert.calledOnceWithExactly(calculateNumberSpy, "SUM", 100, 20);
 
         calculateNumberSpy.restore();
-        consoleLogSpy.restore();
 
     });
 });
