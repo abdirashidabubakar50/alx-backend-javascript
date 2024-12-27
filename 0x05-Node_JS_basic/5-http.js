584da660eb2f3da80d5141dd53df30c1d3a5bf10
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-27 18:19:54 +0300

    corrected eslint errors

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,19 +1,19 @@
-const http = require("http");
-const path = require("path");
-const countStudents = require("./3-read_file_async");
+const http = require('http');
+const path = require('path');
+const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Hello Holberton School!");
-    } else if (req.url === "/students") {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      const responseMessage = ["This is the list of our students"];
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      const responseMessage = ['This is the list of our students'];
 
-      const databasePath = path.resolve(process.argv[2] || ""); // Default to empty string if no argument
+      const databasePath = path.resolve(process.argv[2] || ''); // Default to empty string if no argument
 
       const captureOutput = [];
       const originalLog = console.log;
@@ -22,22 +22,22 @@ const app = http.createServer((req, res) => {
       countStudents(databasePath)
         .then(() => {
           responseMessage.push(...captureOutput);
-          res.end(responseMessage.join("\n"));
+          res.end(responseMessage.join('\n'));
         })
         .catch((error) => {
           responseMessage.push(error.message);
-          res.end(responseMessage.join("\n")); // Status code remains 200
+          res.end(responseMessage.join('\n')); // Status code remains 200
         })
         .finally(() => {
           console.log = originalLog;
         });
     } else {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Route not found");
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Route not found');
     }
   } else {
-    res.writeHead(405, { "Content-Type": "text/plain" });
-    res.end("Method not allowed");
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method not allowed');
   }
 });
 
