faed8e64e04b9c344c8e468ff3e2808655b289fe
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-27 16:58:21 +0300

    🎨 Fixed checker errors
    Fixed checker errors

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,19 +1,19 @@
-const http = require('http');
-const path = require('path');
-const countStudents = require('./3-read_file_async');
+const http = require("http");
+const path = require("path");
+const countStudents = require("./3-read_file_async");
 
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      const responseMessage = ['This is the list of our students'];
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Hello Holberton School!");
+    } else if (req.url === "/students") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      const responseMessage = ["This is the list of our students"];
 
-      const databasePath = path.resolve(process.argv[2]);
+      const databasePath = path.resolve(process.argv[2] || ""); // Default to empty string if no argument
 
       const captureOutput = [];
       const originalLog = console.log;
@@ -22,27 +22,27 @@ const app = http.createServer((req, res) => {
       countStudents(databasePath)
         .then(() => {
           responseMessage.push(...captureOutput);
-          res.end(responseMessage.join('\n'));
+          res.end(responseMessage.join("\n"));
         })
         .catch((error) => {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end(error.message);
+          responseMessage.push(error.message);
+          res.end(responseMessage.join("\n")); // Status code remains 200
         })
         .finally(() => {
-          console.log = originalLog; // Restore the original console.log
+          console.log = originalLog;
         });
     } else {
-      res.writeHead(404, 'Content-Type', 'text/plain');
-      res.end('Route not found');
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Route not found");
     }
   } else {
-    res.writeHead(405, 'Content-type', 'text/plain');
-    res.end('Method not allowed');
+    res.writeHead(405, { "Content-Type": "text/plain" });
+    res.end("Method not allowed");
   }
 });
 
 app.listen(port, () => {
-  console.log(`server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
 
 module.exports = app;
