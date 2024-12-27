a925fb73e416fcbbb5feee82dc8fb3a028e09713
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-27 19:44:44 +0300

    fixing eslint errors

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,27 +1,29 @@
+import path from 'path';
 import readDatabase from '../utils';
 
 class StudentsController {
   static async getAllStudents(req, res) {
     try {
-      const databasePath = process.argv[2];
+      const databasePath = path.resolve(process.argv[2]);
 
       const studentsByField = await readDatabase(databasePath);
 
       const responseMessage = ['This is the list of our students'];
 
-      const sortedFields = Object.keys(studentsByField).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+      const sortedFields = Object.keys(studentsByField)
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
 
       sortedFields.forEach((field) => {
         const students = studentsByField[field];
         responseMessage.push(
-          `Number of students in ${field}: ${
-            students.length
-          }. List: ${students.join(', ')}`,
+          `Number of students in ${field}: ${students.length}. `
+            + `List: ${students.join(', ')}`,
         );
       });
-      res.status(200).send(responseMessage.join('\n'));
+
+      return res.status(200).send(responseMessage.join('\n')); // Added return
     } catch (error) {
-      res.status(500).send('Cannot load the database');
+      return res.status(500).send('Cannot load the database');
     }
   }
 
@@ -38,13 +40,13 @@ class StudentsController {
       const studentsByField = await readDatabase(databasePath);
 
       const students = studentsByField[major] || [];
-      const responseMessage = `List: ${students.join(", ")}`;
-      res.status(200).send(responseMessage);
+      const responseMessage = `List: ${students.join(', ')}`;
+
+      return res.status(200).send(responseMessage);
     } catch (error) {
-      res.status(500).send("Cannot load the database");
+      return res.status(500).send('Cannot load the database');
     }
   }
 }
 
 export default StudentsController;
-
