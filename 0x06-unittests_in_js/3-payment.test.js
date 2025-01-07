8fa81d01a5c0d0136edb0a436297c822b78a5841
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-01-07 22:35:20 +0300

    adjustment

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,14 +6,14 @@ const sendPaymentRequestToApi = require('./3-payments')
 describe('sendPaymentRequestToApi', () => {
 
     it('should validate usage of Utils.calculateNumber', () => {
-        const Spy = sinon.spy(Utils, "calculateNumber");
+        const spy = sinon.spy(Utils, "calculateNumber");
 
         sendPaymentRequestToApi(100, 20);
 
-        expect(Spy.calledOnceWithExactly("SUM", 100, 20)).to.be
+        expect(spy.calledOnceWithExactly("SUM", 100, 20)).to.be
           .true;
 
-        Spy.restore();
+        spy.restore();
     });
 });
 
