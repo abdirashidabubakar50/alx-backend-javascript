51584e3a9974f65587e1e47d7e8ee29bd6bcf395
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-01-24 22:05:43 +0300

    spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,16 @@
-const { expect } = require('chai')
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payments')
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
+    it('should call Utils.calculateNumber with correct arguments', () => {
+        const spy = sinon.spy(Utils, 'calculateNumber');
 
-    it('should validate usage of Utils.calculateNumber', () => {
-        const spy = sinon.spy(Utils, "calculateNumber");
+        sendPaymentRequestToApi(100, 20)
 
-        sendPaymentRequestToApi(100, 20);
-
-        expect(spy.calledOnceWithExactly("SUM", 100, 20)).to.be
-          .true;
+        sinon.assert.calledOnce(spy);
+        sinon.assert.calledWith(spy, 'SUM', 100, 20);
 
         spy.restore();
-    });
+    })
 });
-
